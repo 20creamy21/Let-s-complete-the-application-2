3a2b6137c1b362a7a916cd56ecacf268b4d50305
20creamy21 <tsuywaga2021@gmail.com>
2021-03-16 13:56:15 +0000

    [Add]画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ef6dc9e18c25ee659db9196d37a785b9143ab7229608ec1a100c8676166ceaf830f0c19e7a9cf5556a592ee7e33cfea3b884f4910ce71521af369b86b2a8f98a'
